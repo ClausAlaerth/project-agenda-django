09d8819988ea553eebd9f91fec8797c11a7af83e
Lucas Aquino de Oliveira <lc.aquinodeoliveira@gmail.com>
2024-12-11 19:18:47 -0300

    Update: header/partials created

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,12 +8,13 @@ def index(request):
 
     contacts = Contact.objects \
         .filter(show=True) \
-        .order_by("id")[:30]
+        .order_by("-id")[:10]
 
     print(contacts.query)
 
     context = {
         "contacts": contacts,
+        "site_title": "Contatos - ",
     }
 
     return render(
@@ -32,8 +33,11 @@ def contact(request, contact_id):
         show=True,
     )
 
+    site_title = f"{single_contact.first_name} {single_contact.last_name} - "
+
     context = {
         "contact": single_contact,
+        "site_title": site_title,
     }
 
     return render(
