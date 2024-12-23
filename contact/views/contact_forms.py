1ef71a8a9e165d4b4c047b8f19fcb19c85cc8cc0
Lucas Aquino de Oliveira <lc.aquinodeoliveira@gmail.com>
2024-12-23 15:34:46 -0300

    Update: modelform created/connected to html forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,26 +1,63 @@
+from typing import Any
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.db.models import Q
 from contact.models import Contact
+from django.core.exceptions import ValidationError
+
+from django import forms
 
 # Create your views here.
 
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            "first_name",
+            "last_name",
+            "phone",
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            "first_name",
+            ValidationError(
+                "mensagem de erro",
+                code="invalid"
+            )
+        )
+
+        self.add_error(
+            "last_name",
+            ValidationError(
+                "mensagem de erro",
+                code="invalid"
+            )
+        )
+
+        return super().clean()
+
+
 def create(request):
     if request.method == "POST":
-        print()
-        print(request.method)
-        print(request.POST.get("first_name"))
-        print(request.POST.get("last_name"))
-        print()
+        context = {
+            "site_title": "Create Contact - ",
+            "form": ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            "contact/create.html",
+            context=context,
+        )
 
     context = {
-
+        "site_title": "Create Contact - ",
+        "form": ContactForm()
     }
-
-    print()
-    print(request.method)
-    print()
 
     return render(
         request,
