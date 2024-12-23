7323d4b6a6e267da036595c5fdbe31e081724ce1
Lucas Aquino de Oliveira <lc.aquinodeoliveira@gmail.com>
2024-12-23 15:41:05 -0300

    Update: moved contactforms to module forms.py

---
+++ contact/forms.py
@@ -0,0 +1,34 @@
+from django.core.exceptions import ValidationError
+from django import forms
+from contact.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            "first_name",
+            "last_name",
+            "phone",
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            "first_name",
+            ValidationError(
+                "mensagem de erro",
+                code="invalid"
+            )
+        )
+
+        self.add_error(
+            "last_name",
+            ValidationError(
+                "mensagem de erro",
+                code="invalid"
+            )
+        )
+
+        return super().clean()
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,44 +1,9 @@
-from typing import Any
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from django.db.models import Q
-from contact.models import Contact
-from django.core.exceptions import ValidationError
+from django.shortcuts import render
 
-from django import forms
+from contact.forms import ContactForm
+
 
 # Create your views here.
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            "first_name",
-            "last_name",
-            "phone",
-        )
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            "first_name",
-            ValidationError(
-                "mensagem de erro",
-                code="invalid"
-            )
-        )
-
-        self.add_error(
-            "last_name",
-            ValidationError(
-                "mensagem de erro",
-                code="invalid"
-            )
-        )
-
-        return super().clean()
 
 
 def create(request):
